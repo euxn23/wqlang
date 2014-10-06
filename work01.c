61b203c05db1abe137ba172427d9ee75161c8ba5
Yuta.SZK <diesiraeinside@gmail.com>
2014-10-07 02:39:38 +0900

    init func developed

--- work01.c
+++ work01.c
@@ -1,5 +1,5 @@
 #include "stdio.h"
-// #include "string.h"
+#include "string.h"
 // #include "stdlib.h"
 
 void kmp_init(char* patt);
@@ -14,27 +14,27 @@ int main(void)
 }
 
 void kmp_init(char* patt){
-  int len = sizeof(patt);
-  char back[len];
+  int m = strlen(patt);
+  char back[m];
   int i = 0, j = -1;
+  back[0] = -1;
 
-  while(j >= 0) {
-    if(patt[i] != patt[j]){
-      j++;
+  while(i < m-1) {
+    if((j >= 0) && (patt[i] != patt[j])){
       j = back[j];
     }
     i++;
+    j++;
+    back[i] = j;
   }
 
-
-
-
-  printf("%d\n", len);
-  printf("%s\n", patt);
+  for(i=0;i<m;i++){
+    printf("%d\n", back[i]);
+  }
   printf("%p\n", patt);
   // printf("%s\n", src);
 }
 
-void kmp_exec(char* text, char* patt){
+void kmp_match(char* text, char* patt){
 
 }
