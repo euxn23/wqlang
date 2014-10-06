8eb2a0dbc796120169a4a866cb1809a8098b622a
Yuta.SZK <diesiraeinside@gmail.com>
2014-10-07 03:36:16 +0900

    finish

--- work01.c
+++ work01.c
@@ -1,22 +1,35 @@
 #include "stdio.h"
 #include "string.h"
-// #include "stdlib.h"
 
-void kmp_init(char* patt);
+void kmp_init(char* patt, int back[]);
 
-void kmp_exec(char* text, char* patt);
+void kmp_match(char* patt, char* text, int back[]);
 
-int main(void)
+int main(int argc, char *argv[])
 {
+  /* パターン、テキストを入力 */
+  // char patt[1024];
+  // char text[1024];
+  // printf("Pattern: ");
+  // scanf("%s", patt);
+  // printf("Text: ");
+  // scanf("%s", text);
+
+  /* テストデータ */
   char patt[] = "abcabcab";
-  kmp_init(patt);
+  //=> back = [-1, 0, 0, 0, 1, 2, 3, 4]
+  char text[] = "abcababcabccabcabcacbacacbacbacabcabcbacbacbabcabcbabbbacbabcbacacbacabcabcaba";
+
+  int m = strlen(patt);
+  int back[m];
+  kmp_init(patt, back);
+  kmp_match(patt, text, back);
   return 0;
 }
 
-void kmp_init(char* patt){
-  int m = strlen(patt);
-  char back[m];
+void kmp_init(char* patt, int back[]){
   int i = 0, j = -1;
+  int m = strlen(patt);
   back[0] = -1;
 
   while(i < m-1) {
@@ -28,13 +41,30 @@ void kmp_init(char* patt){
     back[i] = j;
   }
 
-  for(i=0;i<m;i++){
-    printf("%d\n", back[i]);
+  /* backの内容を確認 */
+  int k;
+  for (k=0;k<m;k++) {
+    printf("back%d: %d\n", k, back[k]);
   }
-  printf("%p\n", patt);
-  // printf("%s\n", src);
+
 }
 
-void kmp_match(char* text, char* patt){
+void kmp_match(char* patt, char* text, int back[]){
+  int i = 0, j = 0;
+  int m = strlen(patt), n = strlen(text);
+
+  while ((i < n) && (j < m)) {
+    while ((j >= 0) && (text[i] != patt[j])) {
+      j = back[j];
+    }
+    i++;
+    j++;
+  }
+
+  if (j == m) {
+    printf("パターンは含まれます: %d文字目～%d文字目\n", i-j+1, i-j+m);
+  } else {
+    printf("パターンは含まれませんでした\n");
+  }
 
 }
